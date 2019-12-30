c6815647f97ecbc679137a38bbfd2804a1ac998f
Sg <shogo1104@gmail.com>
2019-12-30 23:23:45 +0900

    Added a minimal tester

---
+++ contracts/tests/simple_tester.js
@@ -0,0 +1,92 @@
+const chalk = require("chalk")
+const figlet = require("figlet")
+const inquirer = require("inquirer")
+const clear = require("clear")
+const clui = require("clui")
+const shell = require("shelljs/global")
+const child_process = require("child_process")
+var argv = require('minimist')(process.argv.slice(2));
+const ALL_PHRASE = "Test All✋"
+let currentDir = pwd().toString().split("/").pop()
+let TEST_BASEDIR = currentDir === "contracts" ? "./tests" : "./contracts/tests"
+
+clear()
+console.log(
+  chalk.yellow(figlet.textSync("ovm.tz", { horizontalLayout: 'full' }))
+)
+process.stdout.write('\n');
+
+const Spinner = clui.Spinner
+let countdown = new Spinner(`Test Running...`, ['◜','◠','◝','◞','◡','◟']);
+let testfiles = ls(TEST_BASEDIR).filter(name=>{
+  return name.indexOf("tester.sh") > 0
+})
+testfiles.unshift(ALL_PHRASE)
+
+
+;(
+  (argv._[0] == "all") ? 
+    skip(testfiles)
+  :
+    ask(testfiles)
+)
+.then(res=>{
+  countdown.start();
+  let i = 1
+  let pid = setInterval(_=>{
+    if (i === 0) {
+      process.stdout.write('\n');
+      countdown.stop()
+
+      let selectedTestfiles;
+      clearInterval(pid)
+      if(res.selectedTestfiles[0] == ALL_PHRASE){
+        testfiles.shift()
+        selectedTestfiles = testfiles;
+      } else {
+        selectedTestfiles = res.selectedTestfiles
+      }
+      selectedTestfiles.map(name=>{
+        let shellResult = child_process.spawnSync(`bash`, [`${TEST_BASEDIR}/${name}`], {stdio: 'inherit'})
+        shellResult.command = name
+        return shellResult
+      }).map(res=>{
+        if(res.status !== 0) {
+          echo(chalk.white.bgRed(`[[[ Failed: ${res.command} ]]]`));
+          echo(chalk.red(`Stderr: ${res.stderr.toString()}`));
+        } else {
+          echo(chalk.white.bgGreen(`[[[ Passed: ${res.command} ]]]`));
+          echo(chalk.green(`Stdout: ${res.stdout.toString()}`));
+        }
+      })
+    }
+    i--;
+  }, 1000)
+}).catch(err=>{ console.error(err) })
+
+
+
+
+function ask(testfiles){
+  return inquirer.prompt([
+    {
+      type: 'checkbox',
+      message: 'Which will you test?',
+      name: 'selectedTestfiles',
+      choices: testfiles,
+      validate: function(answer) {
+        if (answer.length < 1) {
+          return 'You must choose at least one test.';
+        }
+        return true;
+      }
+    }
+  ])
+}
+
+function skip(testfiles){
+  console.log(testfiles[0]);
+  return new Promise((resolve, reject)=>{
+    resolve({ selectedTestfiles: [testfiles[0]] })
+  })
+}
