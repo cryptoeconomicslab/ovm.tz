62567663902f015d41f089984503a814268dc751
Shuhei Hiya <syuhei176@gmail.com>
2020-01-05 22:08:28 +0900

    clean: remove simple tester

--- contracts/tests/test_deposit_002.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-SCRIPT_DIR=$(cd $(dirname ${0}) && pwd)
-
-# Deposit 1tez and confirm it
-# Diff module will take the diff of deposit
-# But merkle interval tree isn't implemented in this contract
-ligo dry-run contracts/main.ligo main "`echo $(cat ${SCRIPT_DIR}/params/param_deposit_001)`" "`echo $(cat ${SCRIPT_DIR}/storages/initial_storage)`" --amount=1
--- contracts/tests/test_getstorage_001.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-SCRIPT_DIR=$(cd $(dirname ${0}) && pwd)
-ligo dry-run contracts/main.ligo main "`echo $(cat ${SCRIPT_DIR}/params/param_getstorage_001)`" "`echo $(cat ${SCRIPT_DIR}/storages/initial_storage)`" --amount=1
