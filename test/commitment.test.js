97dd24adf80a3ea907f7df455c315e1bbd379a50
Shuhei Hiya <syuhei176@gmail.com>
2020-01-05 22:19:15 +0900

    test: add failing test of submit #26

--- test/commitment.test.js
+++ test/commitment.test.js
@@ -6,7 +6,8 @@ describe('CommitmentContract', function() {
   this.timeout(5000)
 
   describe('submit', () => {
-    const testParams = rmWhiteSpaces(`Submit(
+    it('suceed to submit', () => {
+      const testParams = rmWhiteSpaces(`Submit(
         record
           block_number = 1n;
           block_number_string = "1";
@@ -14,13 +15,30 @@ describe('CommitmentContract', function() {
         end
       )`)
 
-    it('suceed to submit', () => {
       const result = invokeTest({
         parameter: testParams,
         initialStorage,
         source: 'tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV'
       })
       assert.equal(result.status, 'ok')
+    })
+
+    it('fail to submit', () => {
+      const testParams = rmWhiteSpaces(`Submit(
+        record
+          block_number = 2n;
+          block_number_string = "2";
+          root = "root1";
+        end
+      )`)
+
+      assert.throws(() => {
+        invokeTest({
+          parameter: testParams,
+          initialStorage,
+          source: 'tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV'
+        })
+      })
     })
   })
 })
