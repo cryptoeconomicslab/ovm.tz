05a92a4e65184ffe30474601a64d4111ed8a2711
Shuhei Hiya <syuhei176@gmail.com>
2020-01-08 11:33:17 +0900

    feat: add deploy script

--- script/build.js
+++ script/build.js
@@ -1,7 +1,6 @@
 const childProcess = require('child_process')
 const path = require('path')
 const fs = require('fs')
-
 const { initialStorage } = require('../test/helper/utils')
 
 function getCommand({
@@ -10,6 +9,14 @@ function getCommand({
   entryPoint = 'main'
 }) {
   return `ligo compile-contract ${contractPath} ${entryPoint}`
+}
+
+function getCompileStorageCommand({
+  initialStorage,
+  contractPath = 'contracts/main.ligo',
+  entryPoint = 'main'
+}) {
+  return `ligo compile-storage ${contractPath} ${entryPoint} '${initialStorage}'`
 }
 
 function build({ output = path.join(__dirname, '../build') }) {
@@ -21,12 +28,22 @@ function build({ output = path.join(__dirname, '../build') }) {
       })
     )
     .toString()
+  const resultStorage = childProcess
+    .execSync(
+      getCompileStorageCommand({
+        initialStorage,
+        entryPoint: 'main'
+      })
+    )
+    .toString()
+
   try {
     fs.mkdirSync(output)
   } catch (e) {
     console.error(e)
   }
   fs.writeFileSync(path.join(output, 'main.tz'), result)
+  fs.writeFileSync(path.join(output, 'storage.tz'), resultStorage)
 }
 
 build({})
