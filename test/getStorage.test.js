2fab1f94efefc58191fe4765b898793ca96f6f1a
Sg <shogo1104@gmail.com>
2020-01-10 20:19:00 +0900

    getStorage's test has been exceeded 5000ms and extended that timeout constant

--- test/getStorage.test.js
+++ test/getStorage.test.js
@@ -3,7 +3,7 @@ const invokeTest = require('./helper/invokeTest')
 const { rmWhiteSpaces, initialStorage } = require('./helper/utils')
 
 describe('getStorage', function() {
-  this.timeout(5000)
+  this.timeout(10000)
 
   describe('getStorage', () => {
     const testParams = rmWhiteSpaces(`GetStorage(0)`)
