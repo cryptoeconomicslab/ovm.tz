3e70edf16751c14b5e076c5be7050d0246d9b475
Sg <shogo1104@gmail.com>
2020-01-11 20:02:26 +0900

    Add property record tests

--- test/codec.test.js
+++ test/codec.test.js
@@ -14,50 +14,105 @@ describe('CodecContract', function() {
   this.timeout(20000)
 
   describe('Codec', () => {
-    it('packs a record', async () => {
-      const packParam = `record
+    describe('bytes_pack', () => {
+      it('packs a record', async () => {
+        const packParam = `record
         attributeA = ( "hoge" : string );
         attributeB = 1n;
       end`
-      const result = await invokeTest({
-        contractPath: CONTRACT_PATH,
-        parameter: packParam,
-        entryPoint: 'pack_sample_record',
-        initialStorage: '("000000": bytes)'
+        const result = await invokeTest({
+          contractPath: CONTRACT_PATH,
+          parameter: packParam,
+          entryPoint: 'pack_sample_record',
+          initialStorage: '("00": bytes)'
+        })
+        assert.deepEqual(result.status, STATUS.OK)
+        assert.deepEqual(result.postState, `0x0507070100000004686f67650001`)
       })
-      assert.deepEqual(result.status, STATUS.OK)
-      assert.deepEqual(result.postState, `0x0507070100000004686f67650001`)
+
+      it('packs a property record', async () => {
+        const packParam = `record
+          predicate_address = ("tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV" : address);
+          inputs = list ("0001" : bytes); ("0002" : bytes); end;
+        end`
+        const result = await invokeTest({
+          contractPath: CONTRACT_PATH,
+          parameter: packParam,
+          entryPoint: 'pack_property_record',
+          initialStorage: `("00": bytes)`
+        })
+        assert.deepEqual(
+          result.postState,
+          '0x050707020000000e0a0000000200010a0000000200020a00000016000053c1edca8bd5c21c61d6f1fd091fa51d562aff1d'
+        )
+      })
+
+      it('packs a tuple', async () => {
+        const packParam = `(
+        ("tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV" : address),
+        list ("0001" : bytes); ("0002" : bytes); end
+      )`
+        const result = await invokeTest({
+          contractPath: CONTRACT_PATH,
+          parameter: packParam,
+          entryPoint: 'pack_tuple',
+          initialStorage: `("00": bytes)`
+        })
+        assert.deepEqual(
+          result.postState,
+          '0x0507070a00000016000053c1edca8bd5c21c61d6f1fd091fa51d562aff1d020000000e0a0000000200010a000000020002'
+        )
+      })
     })
 
-    it('unpacks a binary', async () => {
-      const unpackParam = `("0507070100000004686f67650001": bytes)`
-      const result = await invokeTest({
-        contractPath: CONTRACT_PATH,
-        parameter: unpackParam,
-        entryPoint: 'unpack_sample_record',
-        initialStorage: '(None: option(sample_record))'
+    describe('bytes_unpack', () => {
+      it('unpacks a record', async () => {
+        const unpackParam = `("0507070100000004686f67650001": bytes)`
+        const result = await invokeTest({
+          contractPath: CONTRACT_PATH,
+          parameter: unpackParam,
+          entryPoint: 'unpack_sample_record',
+          initialStorage: '(None: option(sample_record))'
+        })
+
+        assert.deepEqual(result.status, STATUS.OK)
+        assert.deepEqual(result.postState, [
+          'SOME',
+          { attributeA: 'hoge', attributeB: 1 }
+        ])
       })
 
-      assert.deepEqual(result.status, STATUS.OK)
-      assert.deepEqual(result.postState, [
-        'SOME',
-        { attributeA: 'hoge', attributeB: 1 }
-      ])
-    })
-
-    it('unpack tuple', async () => {
-      const packParam = `("0507070a00000016000053c1edca8bd5c21c61d6f1fd091fa51d562aff1d020000000e0a0000000200010a000000020002":bytes)`
-      const result = await invokeTest({
-        contractPath: CONTRACT_PATH,
-        parameter: packParam,
-        entryPoint: 'unpack_tuple',
-        initialStorage: `(None: option(sample_tuple))`
+      it('unpacks a property record', async () => {
+        const packParam = `("050707020000000e0a0000000200010a0000000200020a00000016000053c1edca8bd5c21c61d6f1fd091fa51d562aff1d":bytes)`
+        const result = await invokeTest({
+          contractPath: CONTRACT_PATH,
+          parameter: packParam,
+          entryPoint: 'unpack_property_record',
+          initialStorage: `(None: option(property_record))`
+        })
+        assert.deepEqual(result.postState, [
+          'SOME',
+          {
+            predicate_address: 'tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV',
+            inputs: ['0x0001', '0x0002']
+          }
+        ])
       })
-      assert.deepEqual(result.status, STATUS.OK)
-      assert.deepEqual(result.postState, [
-        'SOME',
-        ['tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV', ['0x0001', '0x0002']]
-      ])
+
+      it('unpacks a tuple', async () => {
+        const packParam = `("0507070a00000016000053c1edca8bd5c21c61d6f1fd091fa51d562aff1d020000000e0a0000000200010a000000020002":bytes)`
+        const result = await invokeTest({
+          contractPath: CONTRACT_PATH,
+          parameter: packParam,
+          entryPoint: 'unpack_tuple',
+          initialStorage: `(None: option(sample_tuple))`
+        })
+        assert.deepEqual(result.status, STATUS.OK)
+        assert.deepEqual(result.postState, [
+          'SOME',
+          ['tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV', ['0x0001', '0x0002']]
+        ])
+      })
     })
   })
 })
--- test/contracts/codec.ligo
+++ test/contracts/codec.ligo
@@ -4,13 +4,26 @@ type sample_record is record
   attributeA: string;
   attributeB: nat;
 end
+type property_record is record
+  predicate_address: address;
+  inputs: list(bytes);
+end
 type sample_tuple is (address * list(bytes))
 
 function pack_sample_record (const pack_params: sample_record; const s: bytes) : ( ops * bytes ) is
 begin skip end with ( (nil:ops) , bytes_pack(pack_params) )
 
+function pack_property_record (const action: property_record; const s: bytes) : (ops * bytes) is
+begin skip end with ((nil:ops), bytes_pack(action))
+
+function pack_tuple (const action: sample_tuple; const s: bytes) : (ops * bytes) is
+begin skip end with ((nil:ops), bytes_pack(action))
+
 function unpack_sample_record (const unpack_params: bytes; const s: option(sample_record)) : ( ops * option(sample_record) ) is
 begin skip end with ( (nil:ops) , ( bytes_unpack( unpack_params ) : option(sample_record) ) )
+
+function unpack_property_record (const action: bytes; const s: option(property_record)) : (ops * option(property_record)) is
+begin skip end with ((nil:ops), ( bytes_unpack(action) : option(property_record ) ) )
 
 function unpack_tuple (const action: bytes; const s: option(sample_tuple)) : (ops * option(sample_tuple)) is
 begin skip end with ( (nil:ops), ( bytes_unpack(action) : option(sample_tuple) ) )
