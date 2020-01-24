24119ca48c6869305191fbd4b78113eefebbafa3
Shuhei Hiya <syuhei176@gmail.com>
2020-01-25 01:33:04 +0900

    feat: add verify_inclusion #32

--- test/commitment.test.js
+++ test/commitment.test.js
@@ -53,4 +53,45 @@ describe('CommitmentContract', function() {
       assert.deepEqual(result.failwith, 'block_number should be next block')
     })
   })
+
+  describe('verify_inclusion', () => {
+    it('suceed to verify inclusion', async () => {
+      const initialStorage = rmWhiteSpaces(`False`)
+      const testParams = rmWhiteSpaces(`(
+        ("0000": bytes),
+        ("tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV": address),
+        record
+          start_ = 0n;
+          end_ = 1n;
+        end,
+        record
+          address_inclusion_proof = record
+            leaf_index = ("tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV": address);
+            leaf_position = 0n;
+            siblings = (map end : map(nat, address_tree_node));
+          end;
+          interval_inclusion_proof = record
+            leaf_index = 0n;
+            leaf_position = 0n;
+            siblings = (map
+              0n -> record
+                data = ("0000": bytes);
+                start = 0n;
+              end
+            end : map(nat, interval_tree_node));
+          end;
+        end,
+        ("0000": bytes)
+      )`)
+
+      const result = await invokeTest({
+        contractPath: 'contracts/test.ligo',
+        entryPoint: 'test_verify_inclusion',
+        parameter: testParams,
+        initialStorage,
+        source: 'tz1TGu6TN5GSez2ndXXeDX6LgUDvLzPLqgYV'
+      })
+      assert.deepEqual(result.postState, 'true')
+    })
+  })
 })
